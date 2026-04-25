7ae02958677aa4625629a41aa61c661022c38b23
jugovi18 <juliangomezvillamayor@gmail.com>
2026-04-25 23:49:05 +0200

    Añadir script de chequeo de servicios con logging

---
+++ chequeo_servicios.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p ~/laboratorio/logs/
+
+LOG=~/laboratorio/logs/script.log
+log (){
+	echo "$(date "+%Y-%m-%d %H:%M:%S") -$1 " >> $LOG
+}
+
+for servicio in ssh cron ufw; do
+    status=$(systemctl is-active $servicio 2>/dev/null)
+        log " El $servicio está $status"
+done
