7311d72833b4cbb60c982e6fe137b11b81f55a79
jugovi18 <juliangomezvillamayor@gmail.com>
2026-04-25 23:54:42 +0200

    Añadido el servicio fail2ban

--- chequeo_servicios.sh
+++ chequeo_servicios.sh
@@ -7,7 +7,7 @@ log (){
 	echo "$(date "+%Y-%m-%d %H:%M:%S") -$1 " >> $LOG
 }
 
-for servicio in ssh cron ufw; do
+for servicio in ssh cron ufw fail2ban; do
     status=$(systemctl is-active $servicio 2>/dev/null)
         log " El $servicio está $status"
 done
